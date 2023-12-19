5a659e42a6711826c83c92d7abb803fedc112616
deviantschemist <michaelngo1337@yahoo.com>
2023-12-18 23:58:35 -0800

    make print report functionality

--- main.py
+++ main.py
@@ -9,7 +9,10 @@ def count_letter_occurrence(text):
     if not char.isalpha() or char in answer:
       continue
     answer[char] = text.lower().count(char)
-  return answer
+  tuple_list = sorted(answer.items(), key=lambda x: x[1], reverse=True)
+  for item in tuple_list:
+    print(f"The '{item[0]}' was found {item[1]} times")
+
 
 with open("books/frankenstein.txt") as f:
   file_contents = f.read()
